c2e54d40b352c134cccd67c6d6d7bea78d677d8d
Viacheslav Yuskevych <viacheslav.yuskevych@globallogic.com>
2023-03-24 16:21:07 +0200

    header

---
+++ src/layouts/Header.tsx
@@ -0,0 +1,11 @@
+import styles from '../styles/header.module.css';
+
+function Header() {
+  return (
+    <header className={styles.header}>
+      <h1 className={styles.h1}>React app</h1>
+    </header>
+  );
+}
+
+export default Header;
