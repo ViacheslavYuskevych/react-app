d67b1c2c651409048908c9ed10c8a0dea26db0fa
Viacheslav Yuskevych <viacheslav.yuskevych@globallogic.com>
2023-04-03 17:56:01 +0300

    Router

--- src/layouts/Main.tsx
+++ src/layouts/Main.tsx
@@ -1,10 +1,18 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import TodoPage from '../pages/TodoPage';
 import styles from '../styles/main.module.css';
+import AuthPage from '../auth/AuthPage';
 
 function Main() {
   return (
     <main className={styles.main}>
-      <TodoPage></TodoPage>
+      <Router>
+        <Routes>
+          <Route path='/' element={<TodoPage />} />
+          <Route path='/auth' element={<AuthPage />} />
+        </Routes>
+      </Router>
     </main>
   );
 }
